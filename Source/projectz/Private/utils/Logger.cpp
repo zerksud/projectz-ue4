5613c3c528efc55183fbf5e4d8b1a8e24c4ca163
zerksud <zerksud@gmail.com>
2014-04-29 22:43:18 +0400

    move Logger to projectz::utils namespace

--- Source/projectz/Private/utils/Logger.cpp
+++ Source/projectz/Private/utils/Logger.cpp
@@ -2,16 +2,18 @@
 #include "Logger.h"
 
 namespace projectz {
+    namespace utils {
 
-    Logger::Logger() {
-    }
+        Logger::Logger() {
+        }
 
-    Logger::~Logger() {
-    }
+        Logger::~Logger() {
+        }
 
-    void Logger::print(const char* message) {
-        if (GEngine) {
-            GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, FString(message));
+        void Logger::print(const char* message) {
+            if (GEngine) {
+                GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, FString(message));
+            }
         }
     }
 }
--- Source/projectz/Private/utils/Logger.h
+++ Source/projectz/Private/utils/Logger.h
@@ -1,20 +1,21 @@
 #pragma once
 
-#define LOGD(message)   (projectz::Logger::getInstance().print(message))
+#define LOGD(message)   (projectz::utils::Logger::getInstance().print(message))
 
 namespace projectz {
+    namespace utils {
+        class Logger {
+        public:
+            static Logger& getInstance() {
+                static Logger instance;
+                return instance;
+            }
 
-    class Logger {
-    public:
-        static Logger& getInstance() {
-            static Logger instance;
-            return instance;
-        }
+            void print(const char* message);
 
-        void print(const char* message);
-
-    private:
-        Logger();
-        ~Logger();
-    };
+        private:
+            Logger();
+            ~Logger();
+        };
+    }
 }
