a3f3b6b11438d512e83670c773b4d8ae8ac1e453
zerksud <zerksud@gmail.com>
2014-05-04 22:32:36 +0400

    fix cookAll.bat to be runnable from anywhere

--- cookAll.bat
+++ cookAll.bat
@@ -1,1 +1,1 @@
-UE4Editor-Cmd.exe %cd%\projectz.uproject -run=cook -targetplatform=WindowsNoEditor -CookAll
+UE4Editor-Cmd.exe %~dp0\projectz.uproject -run=cook -targetplatform=WindowsNoEditor -CookAll
