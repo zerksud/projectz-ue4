d1826e2760b8d17b210247008ba73d98ca888519
zerksud <zerksud@gmail.com>
2014-05-11 19:02:54 +0400

    set explicit Type for GTest module

--- Source/ThirdParty/GTest.Build.cs
+++ Source/ThirdParty/GTest.Build.cs
@@ -3,6 +3,8 @@ using System.IO;
 
 public class GTest : ModuleRules {
     public GTest(TargetInfo Target) {
+        Type = ModuleType.External;
+
         ModulePath = Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name));
         GTestPath = Path.Combine(ModulePath, "../../../gtest/");
 
