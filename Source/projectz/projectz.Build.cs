bd823e83bf0d47093dfecf22da065150abfcc3aa
zerksud <zerksud@gmail.com>
2014-05-12 23:56:10 +0400

    reformat projectz.Build.cs and remove commented code

--- Source/projectz/projectz.Build.cs
+++ Source/projectz/projectz.Build.cs
@@ -1,26 +1,9 @@
-
-
 using UnrealBuildTool;
 
-public class projectz : ModuleRules
-{
-	public projectz(TargetInfo Target)
-	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+public class projectz : ModuleRules {
+    public projectz(TargetInfo Target) {
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
         PrivateDependencyModuleNames.AddRange(new string[] { "projectzCore" });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.Add("Slate");
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-		// if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
-		// {
-		//		if (UEBuildConfiguration.bCompileSteamOSS == true)
-		//		{
-		//			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
-		//		}
-		// }
-	}
+    }
 }
