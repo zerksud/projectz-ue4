2e16e758e534dd474222786eaeea02e32f9c7bb3
zerksud <zerksud@gmail.com>
2014-05-17 03:56:54 +0400

    add more tests for ZDictionary

--- Source/projectzTest/Private/utils/DictionaryTest.cpp
+++ Source/projectzTest/Private/utils/DictionaryTest.cpp
@@ -6,17 +6,20 @@
 
 namespace prz {
     namespace testing {
-        class DictionaryTestWithInstance: public ::testing::Test {
+        class DictionaryTestWithInstance : public ::testing::Test {
         protected:
             static const char* kSomeKey;
+            static const char* kAnotherKey;
             static const int kSomeInt = 42;
+            static const int kAnotherInt = 1337;
             static const bool kSomeBool = true;
 
             utils::ZDictionary dict;
         };
 
         const char* DictionaryTestWithInstance::kSomeKey = "some_key";
-        
+        const char* DictionaryTestWithInstance::kAnotherKey = "another_key";
+
         TEST_F(DictionaryTestWithInstance, IntCanBeStoredAndRetrievedByKey) {
             dict[kSomeKey] = kSomeInt;
 
@@ -29,11 +32,51 @@ namespace prz {
             ASSERT_EQ(kSomeBool, dict[kSomeKey].AsBool());
         }
 
-        TEST_F(DictionaryTestWithInstance, DictCanBeCopiedByAssignmentOperator) {
+        TEST_F(DictionaryTestWithInstance, DictionaryCanBeStoredAndRetrievedByKey) {
+            dict[kSomeKey] = utils::ZDictionary();
+            dict[kSomeKey][kAnotherKey] = kSomeInt;
+
+            ASSERT_EQ(kSomeInt, dict[kSomeKey][kAnotherKey].AsInt());
+        }
+
+        TEST_F(DictionaryTestWithInstance, CopyConstructor_CopiesDictionary) {
             dict[kSomeKey] = kSomeInt;
             utils::ZDictionary anotherDict(dict);
 
             ASSERT_EQ(kSomeInt, anotherDict[kSomeKey].AsInt());
+        }
+
+        TEST_F(DictionaryTestWithInstance, CopyConstructor_CreatesIndependentCopyOfDictionary) {
+            dict[kSomeKey] = kSomeInt;
+            utils::ZDictionary anotherDict = dict;
+            dict[kSomeKey] = kSomeBool;
+
+            ASSERT_EQ(kSomeInt, anotherDict[kSomeKey].AsInt());
+        }
+
+        TEST_F(DictionaryTestWithInstance, AssignmentOperator_CopiesDictionary) {
+            dict[kSomeKey] = kSomeInt;
+            utils::ZDictionary anotherDict;
+            anotherDict = dict;
+
+            ASSERT_EQ(kSomeInt, anotherDict[kSomeKey].AsInt());
+        }
+
+        TEST_F(DictionaryTestWithInstance, AssignmentOperator_CreatesIndependentCopyOfDictionary) {
+            dict[kSomeKey] = kSomeInt;
+            utils::ZDictionary anotherDict;
+            anotherDict = dict;
+            dict[kSomeKey] = kSomeBool;
+
+            ASSERT_EQ(kSomeInt, anotherDict[kSomeKey].AsInt());
+        }
+
+        TEST_F(DictionaryTestWithInstance, ArraySubscriptOperator_RetrievesDictByExactKey) {
+            dict[kSomeKey] = kSomeInt;
+            dict[kAnotherKey] = kAnotherInt;
+
+            ASSERT_EQ(kSomeInt, dict[kSomeKey].AsInt());
+            ASSERT_EQ(kAnotherInt, dict[kAnotherKey].AsInt());
         }
     }
 }
