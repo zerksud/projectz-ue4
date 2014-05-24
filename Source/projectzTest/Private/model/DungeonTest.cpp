93ad17a32a28f851dd9d3aa84e8eab4670407078
zerksud <zerksud@gmail.com>
2014-05-25 01:31:27 +0400

    change 'someMap' and simplify simplest test for ZDungeon

--- Source/projectzTest/Private/model/DungeonTest.cpp
+++ Source/projectzTest/Private/model/DungeonTest.cpp
@@ -9,26 +9,24 @@ namespace prz {
         class DungeonTest : public ::testing::Test {
         protected:
             const static mdl::ZDungeon::ZMapCell* kSomeMap;
-            const static int kSomeMapWidth = 3;
+            const static int kSomeMapWidth = 5;
             const static int kSomeMapHeight = 3;
             const static mdl::ZPosition kSomeMapSomeEmptyCell;
             const static mdl::ZPosition kSomeMapSomeSolidCell;
         };
 
         const mdl::ZDungeon::ZMapCell* DungeonTest::kSomeMap = ""
-            "###"
-            "#.#"
-            "###";
-        const mdl::ZPosition DungeonTest::kSomeMapSomeEmptyCell = mdl::ZPosition(1, 1);
+            "#####"
+            "#<.>#"
+            "#####";
+        const mdl::ZPosition DungeonTest::kSomeMapSomeEmptyCell = mdl::ZPosition(1, 2);
         const mdl::ZPosition DungeonTest::kSomeMapSomeSolidCell = mdl::ZPosition(2, 1);
             
-        TEST_F(DungeonTest, ConstructorWithMap) {
+        TEST_F(DungeonTest, GetWidthAndGetHeightReturnExpectedValues) {
             mdl::ZDungeon dungeon(kSomeMapWidth, kSomeMapHeight, kSomeMap);
 
             EXPECT_EQ(kSomeMapWidth, dungeon.GetWidth());
             ASSERT_EQ(kSomeMapHeight, dungeon.GetHeight());
-            EXPECT_TRUE(dungeon.CellIsEmpty(kSomeMapSomeEmptyCell));
-            ASSERT_FALSE(dungeon.CellIsEmpty(kSomeMapSomeSolidCell));
         }
     }
 }
