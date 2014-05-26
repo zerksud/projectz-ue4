a2d1a59ba9c176197001485d56099ab57ae6bf96
zerksud <zerksud@gmail.com>
2014-05-27 02:40:19 +0400

    add tests for fail-safe map of ZDungeon

--- Source/projectzTest/Private/model/DungeonTest.cpp
+++ Source/projectzTest/Private/model/DungeonTest.cpp
@@ -4,6 +4,11 @@
 
 #include "model/Dungeon.h"
 
+#define ASSERT_DUNGEON_WITH_FAIL_SAFE_MAP(dungeon) \
+    EXPECT_EQ(kFailSafeMapWidth, dungeon.GetWidth()); \
+    EXPECT_EQ(kFailSafeMapHeight, dungeon.GetHeight()); \
+    ASSERT_TRUE(dungeon.CellIsEmpty(kFailSafeMapEmptyCell));
+
 namespace prz {
     namespace testing {
         class DungeonTest : public ::testing::Test {
@@ -13,6 +18,13 @@ namespace prz {
             const static int kSomeMapHeight = 3;
             const static mdl::ZPosition kSomeMapSomeEmptyCell;
             const static mdl::ZPosition kSomeMapSomeSolidCell;
+
+            const static int kSomeIncorrectMapWidth = 0;
+            const static int kSomeIncorrectMapHeight = -5;
+
+            const static int kFailSafeMapWidth = 3;
+            const static int kFailSafeMapHeight = 3;
+            const static mdl::ZPosition kFailSafeMapEmptyCell;
         };
 
         const mdl::ZDungeon::ZMapCell* DungeonTest::kSomeMap = ""
@@ -21,6 +33,8 @@ namespace prz {
             "#####";
         const mdl::ZPosition DungeonTest::kSomeMapSomeEmptyCell = mdl::ZPosition(1, 2);
         const mdl::ZPosition DungeonTest::kSomeMapSomeSolidCell = mdl::ZPosition(2, 1);
+
+        const mdl::ZPosition DungeonTest::kFailSafeMapEmptyCell = mdl::ZPosition(1, 1);
 
         class DungeonWithSomeMapTest : public DungeonTest {
         protected:
@@ -38,6 +52,24 @@ namespace prz {
         TEST_F(DungeonWithSomeMapTest, GetWidthAndGetHeightReturnExpectedValues) {
             EXPECT_EQ(kSomeMapWidth, mDungeon->GetWidth());
             ASSERT_EQ(kSomeMapHeight, mDungeon->GetHeight());
+        }
+
+        TEST_F(DungeonTest, IncorrectWidthCausesCreationOfDungeonWithFailSafeMap) {
+            mdl::ZDungeon dungeon(kSomeIncorrectMapWidth, kSomeMapHeight, kSomeMap);
+
+            ASSERT_DUNGEON_WITH_FAIL_SAFE_MAP(dungeon);
+        }
+
+        TEST_F(DungeonTest, IncorrectHeightCausesCreationOfDungeonWithFailSafeMap) {
+            mdl::ZDungeon dungeon(kSomeMapWidth, kSomeIncorrectMapHeight, kSomeMap);
+
+            ASSERT_DUNGEON_WITH_FAIL_SAFE_MAP(dungeon);
+        }
+
+        TEST_F(DungeonTest, MapWithSizeInconsistentWithWidthAndHeightCausesCreationOfDungeonWithFailSafeMap) {
+            mdl::ZDungeon dungeon(kSomeMapWidth, kSomeMapHeight + 1, kSomeMap);
+
+            ASSERT_DUNGEON_WITH_FAIL_SAFE_MAP(dungeon);
         }
     }
 }
