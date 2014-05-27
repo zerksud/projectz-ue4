88915e65c49a2868fce657b538d1133d6c608f13
zerksud <zerksud@gmail.com>
2014-05-27 22:36:52 +0400

    add EXPECT_CONTAINS and ASSERT_CONTAINS test macroses

---
+++ Source/projectzTest/Private/TestHelpers.h
@@ -0,0 +1,9 @@
+#include <algorithm>
+
+#include "gtest/gtest.h"
+
+#define EXPECT_CONTAINS(container, value) \
+    EXPECT_TRUE(std::find(container##.begin(), container##.end(), value) != container##.end());
+
+#define ASSERT_CONTAINS(container, value) \
+    ASSERT_TRUE(std::find(container##.begin(), container##.end(), value) != container##.end());
