87c5a3d3b4f266b99f7d24173f9ad29ba14c0328
zerksud <zerksud@gmail.com>
2014-05-29 02:27:40 +0400

    add ASSERT_POSITION_DIFF_EQ macro

--- Source/projectzTest/Private/model/PositionTest.h
+++ Source/projectzTest/Private/model/PositionTest.h
@@ -5,3 +5,7 @@
 #define ASSERT_POSITION_EQ(expectedPosition, actualPosition) \
     EXPECT_EQ(expectedPosition##.GetX(), actualPosition##.GetX()); \
     ASSERT_EQ(expectedPosition##.GetY(), actualPosition##.GetY());
+
+#define ASSERT_POSITION_DIFF_EQ(expectedPositionDiff, actualPositionDiff) \
+    EXPECT_EQ(expectedPositionDiff##.GetdX(), actualPositionDiff##.GetdX()); \
+    ASSERT_EQ(expectedPositionDiff##.GetdY(), actualPositionDiff##.GetdY());
