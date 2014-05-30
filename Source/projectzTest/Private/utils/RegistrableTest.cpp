478aa056b935d2bad7f51560b1b0c5803dff160d
zerksud <zerksud@gmail.com>
2014-05-31 03:59:16 +0400

    add test for ZRegistrable's destructor

--- Source/projectzTest/Private/utils/RegistrableTest.cpp
+++ Source/projectzTest/Private/utils/RegistrableTest.cpp
@@ -2,32 +2,68 @@
 
 #include "utils/Registrable.h"
 
+#include "utils/IUniqueIdRegistry.h"
+#include "utils/Services.h"
+
 namespace prz {
     namespace testing {
 
         class RegistrableTest : public ::testing::Test {
-            protected:
-                static const utl::ZIdType kSomeId = 42;
-
-                utl::ZRegistrable mRegistrable;
+        protected:
+            static const utl::ZIdType kSomeId = 42;
         };
 
-        TEST_F(RegistrableTest, Constructor_SetsNoId) {
+        class RegistrableWithInstanceTest : public RegistrableTest {
+        protected:
+            utl::ZRegistrable mRegistrable;
+        };
+
+        TEST_F(RegistrableWithInstanceTest, Constructor_SetsNoId) {
             ASSERT_EQ(utl::ZRegistrable::kNoId, mRegistrable.GetId());
         }
 
-        TEST_F(RegistrableTest, SetId_StoresPassedId) {
+        TEST_F(RegistrableWithInstanceTest, SetId_StoresPassedId) {
             mRegistrable.SetId(kSomeId);
             ASSERT_EQ(kSomeId, mRegistrable.GetId());
         }
 
-        TEST_F(RegistrableTest, IsRegistered_ReturnsFalseForNoId) {
+        TEST_F(RegistrableWithInstanceTest, IsRegistered_ReturnsFalseForNoId) {
             ASSERT_FALSE(mRegistrable.IsRegistered());
         }
 
-        TEST_F(RegistrableTest, IsRegistered_ReturnsTrueForExplicitlySetId) {
+        TEST_F(RegistrableWithInstanceTest, IsRegistered_ReturnsTrueForExplicitlySetId) {
             mRegistrable.SetId(kSomeId);
             ASSERT_TRUE(mRegistrable.IsRegistered());
+        }
+
+        class UniqueIdRegistryRegistrableTestMock : public utl::IUniqueIdRegistry {
+        public:
+            utl::ZIdType mLastReleasedId = 0;
+
+            virtual bool AssignUniqueId(utl::ZRegistrable* object) override {
+                return true;
+            }
+            virtual bool ReleaseUniqueId(utl::ZRegistrable* object) override {
+                mLastReleasedId = object->GetId();
+                return true;
+            }
+        };
+
+        TEST_F(RegistrableTest, Destructor_ReleasesAssignedUniqueId) {
+            using namespace utl;
+
+            ZRegistrable* object = new utl::ZRegistrable();
+            object->SetId(kSomeId);
+
+            UniqueIdRegistryRegistrableTestMock* registry = new UniqueIdRegistryRegistrableTestMock();
+            ZServices::GetInstance().Register<IUniqueIdRegistry>(registry);
+
+            delete object;
+
+            ZIdType lastReleasedId = registry->mLastReleasedId;
+            ZServices::GetInstance().Unregister<IUniqueIdRegistry>();
+
+            ASSERT_EQ(kSomeId, lastReleasedId);
         }
 
     }
