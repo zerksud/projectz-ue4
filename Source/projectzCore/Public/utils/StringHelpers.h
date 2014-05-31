1b74ae567a3495c8b8e4c54c10abd58c422a9f1d
zerksud <zerksud@gmail.com>
2014-06-01 03:39:25 +0400

    add ZString with Format helper function

---
+++ Source/projectzCore/Public/utils/StringHelpers.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+namespace prz {
+    namespace utl {
+        namespace ZString {
+
+            std::string Format(const char* format, ...);
+
+        }
+    }
+}
---
+++ Source/projectzTest/Private/utils/StringHelpersTest.cpp
@@ -0,0 +1,13 @@
+#include "projectzTestPrivatePCH.h"
+
+#include "utils/StringHelpers.h"
+
+namespace prz {
+    namespace testing {
+        
+        TEST(StringHelpersTest, Format_FormatsString) {
+            ASSERT_EQ("some string with 1 number", utl::ZString::Format("some %s with %d number", "string", 1));
+        }
+
+    }
+}
