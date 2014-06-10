f301ed0f2f978c7c6e290c80ec65062c1a5477cd
zerksud <zerksud@gmail.com>
2014-06-11 03:51:15 +0400

    change field to local variable in przCore.Build.cs file

--- Source/przCore/przCore.Build.cs
+++ Source/przCore/przCore.Build.cs
@@ -3,12 +3,9 @@ using System.IO;
 
 public class przCore : ModuleRules {
     public przCore(TargetInfo Target) {
-        ModulePath = Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name));
-
-        PrivateIncludePaths.Add(Path.Combine(ModulePath, "Private"));
-
         PublicDependencyModuleNames.Add("Core");
-    }
 
-    private readonly string ModulePath;
+        string modulePath = Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name));
+        PrivateIncludePaths.Add(Path.Combine(modulePath, "Private"));
+    }
 }
