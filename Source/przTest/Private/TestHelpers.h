7700929921ef834e8c4bf48fd1a9237b9a492432
zerksud <zerksud@gmail.com>
2014-09-18 23:57:25 +0400

    remove przTest

--- Source/przTest/Private/TestHelpers.h
+++
@@ -1,9 +0,0 @@
-#include <algorithm>
-
-#include "gtest/gtest.h"
-
-#define EXPECT_CONTAINS(container, value) \
-    EXPECT_TRUE(std::find((container).begin(), (container).end(), value) != (container).end());
-
-#define ASSERT_CONTAINS(container, value) \
-    ASSERT_TRUE(std::find((container).begin(), (container).end(), value) != (container).end());
