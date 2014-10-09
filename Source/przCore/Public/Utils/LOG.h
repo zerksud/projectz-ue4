4db4d56c49ee3b29abcf231ce5284d8d257effde
zerksud <zerksud@gmail.com>
2014-10-09 23:49:08 +0400

    change LOGD verbosity level to VeryVerbose

--- Source/przCore/Public/Utils/LOG.h
+++ Source/przCore/Public/Utils/LOG.h
@@ -4,13 +4,13 @@
 #include "Utils/Services.h"
 
 #define LOGD(format, ...) \
-    (GET_SERVICE(prz::utl::ILogger)->Log(ELogVerbosity::Log, __FILE__, __LINE__, FString::Printf(ANSI_TO_TCHAR(format), ##__VA_ARGS__)))
+    (GET_SERVICE(prz::utl::ILogger)->Log(ELogVerbosity::VeryVerbose, __FILE__, __LINE__, FString::Printf(ANSI_TO_TCHAR(format), ##__VA_ARGS__)))
 
 #define LOGE(format, ...) \
     (GET_SERVICE(prz::utl::ILogger)->Log(ELogVerbosity::Error, __FILE__, __LINE__, FString::Printf(ANSI_TO_TCHAR(format), ##__VA_ARGS__)))
 
 #define LOGD_ANSI(...) \
-    (GET_SERVICE(prz::utl::ILogger)->Log(ELogVerbosity::Log, __FILE__, __LINE__, __VA_ARGS__))
+    (GET_SERVICE(prz::utl::ILogger)->Log(ELogVerbosity::VeryVerbose, __FILE__, __LINE__, __VA_ARGS__))
 
 #define LOGE_ANSI(...) \
     (GET_SERVICE(prz::utl::ILogger)->Log(ELogVerbosity::Error, __FILE__, __LINE__, __VA_ARGS__))
