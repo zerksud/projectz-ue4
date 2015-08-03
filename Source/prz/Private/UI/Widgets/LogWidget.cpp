812c964e0834f534f04925d3789b95a4620f691e
zerksud <zerksud@gmail.com>
2015-08-04 01:12:35 +0300

    increase hardcoded log font size to more readable one

--- Source/prz/Private/UI/Widgets/LogWidget.cpp
+++ Source/prz/Private/UI/Widgets/LogWidget.cpp
@@ -8,7 +8,7 @@ void SLogWidget::Construct(const FArguments& InArgs) {
     ChildSlot
     [
         SNew(STextBlock)
-        .Font(FSlateFontInfo(FPaths::GameContentDir() / TEXT("Fonts/Inconsolata-Regular.ttf"), 10))
+        .Font(FSlateFontInfo(FPaths::GameContentDir() / TEXT("Fonts/Inconsolata-Regular.ttf"), 20))
         .Text(this, &SLogWidget::GetLogHistory)
     ];
 }
