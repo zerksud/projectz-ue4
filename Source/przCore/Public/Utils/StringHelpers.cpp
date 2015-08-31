10a465af0a318da9bc9085cad9876bb61e28c443
zerksud <zerksud@gmail.com>
2015-08-31 21:46:15 +0300

    remove stl dependency from string_helpers::Join

--- Source/przCore/Public/Utils/StringHelpers.cpp
+++ Source/przCore/Public/Utils/StringHelpers.cpp
@@ -2,8 +2,7 @@
 #include "Utils/StringHelpers.h"
 
 #include <stdarg.h>
-
-#include <sstream>
+#include <string.h>
 
 #include "Utils/LOG.h"
 
@@ -35,15 +34,35 @@ ZString Format(const char* format, ...) {
 }
 
 ZString Join(const ZList<ZString> &stringList, const ZString &delimeter) {
-    std::stringstream ss;
+    size_t result_length = 0;
+    for (const ZString& str : stringList) {
+        result_length += strlen(str.c_str());
+    }
+    result_length += strlen(delimeter.c_str()) * (stringList.size() - 1) + 1;
+    
+    char* buffer = new char[result_length];
     if (stringList.size() > 0) {
-        ss << stringList.front();
+        const char* first_string = stringList.front().c_str();
+        strcpy(buffer, first_string);
+        char* current_buffer_end = buffer + strlen(first_string);
+        
+        const char* delimeter_str = delimeter.c_str();
+        const size_t delimeter_length = strlen(delimeter_str);
+        
         for (auto i = ++stringList.begin(), end = stringList.end(); i != end; ++i) {
-            ss << delimeter << *i;
+            strcat(current_buffer_end, delimeter_str);
+            current_buffer_end += delimeter_length;
+            
+            const char* str = (*i).c_str();
+            strcat(current_buffer_end, str);
+            current_buffer_end += strlen(str);
         }
     }
 
-    return ss.str();
+    ZString result(buffer);
+    delete[] buffer;
+    
+    return result;
 }
 
 }
